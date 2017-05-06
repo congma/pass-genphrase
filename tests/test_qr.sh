c58e47ad6e614c0eae667a6118af26c9910ffa17
Cong Ma <cong.ma@obspm.fr>
2017-05-06 13:02:53 +0800

    tests: simplify QR code headless test.

--- tests/test_qr.sh
+++ tests/test_qr.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 if [ -n "$TEST_QR_SUPPRESS_DISPLAY" ]; then
-    CMD_ENV='env -u DISPLAY'
-else
-    CMD_ENV='env'
+    unset DISPLAY
 fi
-$CMD_ENV $PASS genphrase -q Test/qr
+$PASS genphrase -q Test/qr
